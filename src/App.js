a03c327c5177235f0141225912fc34b8d5ebe09c
kavee <IT20185770@my.sliit.lk>
2022-05-09 19:11:09 +0530

    pages

--- src/App.js
+++ src/App.js
@@ -16,27 +16,33 @@ import AllMovies from "./components/production_team/pages/allMovies";
 import MovieCardProduction1 from "./components/production_team/cards/movieCardProduction1";
 import MovieCardProduction2 from "./components/production_team/cards/movieCardProduction2";
 import CustomerHeader from "./components/customer/navigation/customerHeader";
+import ProductionTeamHeader from "./components/production_team/navigation/productionTeamHeader";
+import AllMoviesCustomer from "./components/customer/pages/allMoviesCustomer";
+import MovieCardCustomer1 from "./components/customer/cards/movieCardCustomer";
+import MovieCardCustomer2 from "./components/customer/cards/movieCardCustomer2";
 
 function App() {
     return (
         <Router>
             <Routes>
+
+                {/*Customer*/}
                 <Route exact path="/customerSignUp" element={<CustomerSignUp/>}/>
+                <Route exact path="/buyTickets" element={<BuyTicket/>}/>
+                <Route exact path="/allMoviesCustomer" element={<AllMoviesCustomer/>}/>
+                <Route exact path="/customerHeader" element={<CustomerHeader/>}/>
+
+
+                {/*Production Team*/}
+
                 <Route exact path="/addNewShowTime" element={<AddNewShowTime/>}/>
                 <Route exact path="/updateShowTime" element={<UpdateShowTime/>}/>
                 <Route exact path="/addMovie" element={<AddMovie/>}/>
                 <Route exact path="/updateMovie" element={<UpdateMovie/>}/>
-                <Route exact path="/buyTickets" element={<BuyTicket/>}/>
-                <Route exact path="/theaterCard" element={<TheaterCard/>}/>
                 <Route exact path="/allTheaters" element={<AllTheaters/>}/>
-                <Route exact path="/movieCardTheater" element={<MovieCardTheater/>}/>
-                <Route exact path="/movieCardTheater2" element={<MovieCardTheater2/>}/>
                 <Route exact path="/oneTheater" element={<OneTheater/>}/>
-                <Route exact path="/movieCardProduction1" element={<MovieCardProduction1/>}/>
-                <Route exact path="/movieCardProduction2" element={<MovieCardProduction2/>}/>
                 <Route exact path="/allMoviesProduction" element={<AllMovies/>}/>
-                <Route exact path="/customerHeader" element={<CustomerHeader/>}/>
-
+                <Route exact path="/productionHeader" element={<ProductionTeamHeader/>}/>
 
             </Routes>
         </Router>
