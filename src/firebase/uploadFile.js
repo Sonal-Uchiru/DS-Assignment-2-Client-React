9c1d9fbc015312ab44f69770d0e73ff456941b53
sonal-uchiru <sonal123326@gmail.com>
2022-05-11 11:49:17 +0530

    code formatted

--- src/firebase/uploadFile.js
+++ src/firebase/uploadFile.js
@@ -1,14 +1,11 @@
 import { storage } from "./configuration";
 import { ref, uploadBytesResumable,getDownloadURL} from "@firebase/storage";
 
-
 export function uploadFile(file){
     const storageRef = ref(storage, `Movies(images)/${file[0].name}`);
     const uploadTask = uploadBytesResumable(storageRef, file[0]);
-
     return new Promise((resolve, reject) => {
         uploadTask.on(  "state_changed",
-
         (snapshot) => {
             const progress = Math.round(
                 (snapshot.bytesTransferred / snapshot.totalBytes) * 100
@@ -18,12 +15,9 @@ export function uploadFile(file){
                     resolve(url);
                 })
             }
-
         },
         (error) => {
             reject(error);
-
         });
     })
-
 }
