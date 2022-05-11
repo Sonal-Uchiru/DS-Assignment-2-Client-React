2e2f7ae1752b18e39dec907d725e6795c091112a
sonal-uchiru <sonal123326@gmail.com>
2022-05-11 12:13:12 +0530

    emailjs configured

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,6 +1,8 @@
 import React, {useState} from "react";
 import axios from "axios";
 import {uploadFile} from "../firebase/uploadFile";
+import {codeGenerator} from "../generators/codeGenerator";
+import {forgotPasswordEmail} from "../emailjs/forgotPasswordEmail";
 
 
 export default function Test() {
@@ -43,10 +45,22 @@ export default function Test() {
            console.log(err);
        })
     }
+
+    async function click3(){
+        const emailContent = {
+            email : "sonal123326@gmail.com",
+            code: codeGenerator(10)
+        }
+        await forgotPasswordEmail(emailContent).then((res)=>{
+            console.log(res)
+        }).catch((err)=>{
+            console.log(err)
+        })
+    }
     return (
         <div className="addMov">
 
-        <button onClick={()=> click2()}>add</button>
+        <button onClick={()=> click3()}>add</button>
             <input type="file" onChange={(e)=> setFile(e.target.files)}/>
         </div>
     );
--- src/emailjs/forgotPasswordEmail.js
+++ src/emailjs/forgotPasswordEmail.js
@@ -1,15 +1,17 @@
 import emailjs from "emailjs-com";
-export async function forgotPasswordEmail(emailContent){
-    await emailjs
-        .send(
-            "service_d2vcq28", //your service id
-            "template_pcwlvj6", // template id
-            emailContent,
-            "user_TGhnW7M8Z4dNu0PzvbuZ9" //
-        )
-        .then((result) => {
-
-        }).catch((err)=>{
-
+export function forgotPasswordEmail(emailContent){
+    return new Promise((resolve, reject)=>{
+        emailjs
+            .send(
+                "service_d2vcq28", //your service id
+                "template_pcwlvj6", // template id
+                emailContent,
+                "user_TGhnW7M8Z4dNu0PzvbuZ9" //
+            )
+            .then((result) => {
+                resolve(result);
+            }).catch((err)=>{
+            reject(err);
         })
+    })
 }
