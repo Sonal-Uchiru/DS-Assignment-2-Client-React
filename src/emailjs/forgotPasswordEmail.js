19fcfde9a135e12a7f34639dcada1bc9b165ae56
sonal-uchiru <sonal123326@gmail.com>
2022-05-11 12:05:42 +0530

    emailjs installed

--- src/emailjs/forgotPasswordEmail.js
+++ src/emailjs/forgotPasswordEmail.js
@@ -1,3 +1,15 @@
-export function forgotPasswordEmail(){
+import emailjs from "emailjs-com";
+export async function forgotPasswordEmail(emailContent){
+    await emailjs
+        .send(
+            "service_d2vcq28", //your service id
+            "template_pcwlvj6", // template id
+            emailContent,
+            "user_TGhnW7M8Z4dNu0PzvbuZ9" //
+        )
+        .then((result) => {
 
+        }).catch((err)=>{
+
+        })
 }
