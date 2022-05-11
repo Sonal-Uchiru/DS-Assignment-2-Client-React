5e5096756c1f6cbde878d44c1da4577c69ac6567
sonal-uchiru <sonal123326@gmail.com>
2022-05-11 18:41:44 +0530

    prettier added and code formatted

--- src/firebase/uploadFile.js
+++ src/firebase/uploadFile.js
@@ -1,23 +1,25 @@
-import { storage } from "./configuration";
-import { ref, uploadBytesResumable,getDownloadURL} from "@firebase/storage";
+import { storage } from './configuration'
+import { ref, uploadBytesResumable, getDownloadURL } from '@firebase/storage'
 
-export function uploadFile(file){
-    const storageRef = ref(storage, `Movies(images)/${file[0].name}`);
-    const uploadTask = uploadBytesResumable(storageRef, file[0]);
+export function uploadFile(file) {
+    const storageRef = ref(storage, `Movies(images)/${file[0].name}`)
+    const uploadTask = uploadBytesResumable(storageRef, file[0])
     return new Promise((resolve, reject) => {
-        uploadTask.on(  "state_changed",
-        (snapshot) => {
-            const progress = Math.round(
-                (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-            );
-            if(progress >= 100){
-                getDownloadURL(storageRef).then(url => {
-                    resolve(url);
-                })
+        uploadTask.on(
+            'state_changed',
+            (snapshot) => {
+                const progress = Math.round(
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                )
+                if (progress >= 100) {
+                    getDownloadURL(storageRef).then((url) => {
+                        resolve(url)
+                    })
+                }
+            },
+            (error) => {
+                reject(error)
             }
-        },
-        (error) => {
-            reject(error);
-        });
+        )
     })
 }
