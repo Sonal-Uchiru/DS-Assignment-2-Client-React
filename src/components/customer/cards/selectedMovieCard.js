3050109da7a5dd5ba48241e9f1217bd9b251cf4f
ChamodhIdurange <89272288+ChamodhIdurange@users.noreply.github.com>
2022-05-16 00:26:25 +0530

    When deleting movie showtime delete done

--- src/components/customer/cards/selectedMovieCard.js
+++ src/components/customer/cards/selectedMovieCard.js
@@ -31,6 +31,9 @@ export default function SelectedMovieCard(props) {
 
     let [mainImage, setMainImage] = useState("https://redzonekickboxing.com/wp-content/uploads/2017/04/default-image.jpg")
 
+    //Payhere
+
+
     useEffect(()=> {
         setMainImage(movieDetails.image)
     },[])
@@ -113,9 +116,9 @@ export default function SelectedMovieCard(props) {
         var payment = {
             // whether it is a testing environment or not
             sandbox: true,
-            merchant_id: "1219390", // Replace your Merchant ID
-            return_url: undefined, // Important
-            cancel_url: undefined, // Important
+            merchant_id: "1213098", // Replace your Merchant ID
+            return_url: "http://localhost:3000/selectedMovie", // Important
+            cancel_url: "http://localhost:3000/selectedMovie", // Important
             notify_url: "http://sample.com/notify",
             order_id: "MT" + new Date().valueOf(),
             items: "movieObj.movieName",
@@ -147,7 +150,7 @@ export default function SelectedMovieCard(props) {
         Swal.fire({
             icon: "error",
             title: "Oops...",
-            text: "Something went wrong!",
+            text: error,
         });
     };
 
@@ -276,7 +279,7 @@ export default function SelectedMovieCard(props) {
                             <div className="modal-footer border-0">
                                 <div className="row text-center">
                                     <div className="col">
-                                        <button onClick={() => checkValidity(2)} type="button" className="btn5 btn-lg">Buy
+                                        <button onClick={() => buyTicket(true)} type="button" className="btn5 btn-lg">Buy
                                             Tickets
                                         </button>
                                     </div>
@@ -284,6 +287,7 @@ export default function SelectedMovieCard(props) {
                                         <button onClick={() => checkValidity(1)} type="button" className="btn6 btn-lg">Add
                                             to Cart
                                         </button>
+
                                     </div>
                                 </div>
                             </div>
@@ -291,6 +295,8 @@ export default function SelectedMovieCard(props) {
                     </div>
                 </div>
             </div>
+
+
 
         </div>
 
