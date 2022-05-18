7317700b1563cc9361e42d38e81b0101e8a5790f
sonal-uchiru <sonal123326@gmail.com>
2022-05-18 13:21:37 +0530

    security disabled

--- src/private/protected_route.js
+++ src/private/protected_route.js
@@ -3,5 +3,5 @@ import { Navigate } from 'react-router-dom';
 export const Private = ({Component}) => {
     const auth = localStorage.getItem('moon-cinema-token');
 
-    return auth ? <Component/>  : <Navigate to="/customerSignIn" />
+    return true ? <Component/>  : <Navigate to="/" />
 }
